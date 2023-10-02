7fdabf82949a531e7fec46b572ad8ad1b9b0956f
Caique Daniel <caiquedaniel@outlook.com.br>
2023-10-01 22:50:18 -0300

    feat: implementar ConfigModule customizado

--- src/@shared/infrastructure/env-config/env-config.module.ts
+++ src/@shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,26 @@
-import { Module } from '@nestjs/common';
+import path from 'node:path';
+import { DynamicModule, Module } from '@nestjs/common';
+
 import { EnvConfigService } from './env-config.service';
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
 
 @Module({
-  providers: [EnvConfigService]
+  providers: [EnvConfigService],
 })
-export class EnvConfigModule {}
+export class EnvConfigModule extends ConfigModule {
+  static forRoot(options?: ConfigModuleOptions): DynamicModule {
+    return super.forRoot({
+      ...options,
+      envFilePath: [
+        path.join(
+          __dirname,
+          '..',
+          '..',
+          '..',
+          '..',
+          `.env.${process.env.NODE_ENV}`,
+        ),
+      ],
+    });
+  }
+}
