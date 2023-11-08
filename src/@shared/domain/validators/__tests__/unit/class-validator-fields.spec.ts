bcaa941a182fa8e147da60cbecffb3bdba60dc9f
Caique Daniel <caiquedaniel@outlook.com.br>
2023-11-07 21:06:13 -0300

    test: add tests for ClassValidatorFields

---
+++ src/@shared/domain/validators/__tests__/unit/class-validator-fields.spec.ts
@@ -0,0 +1,29 @@
+import * as ClassValidator from 'class-validator';
+
+import { ClassValidatorFields } from '../../class-validator-fields';
+
+class StubClassValidatorFields extends ClassValidatorFields<{
+  field: string;
+}> {}
+
+describe('ClassValidatorFields unit tests', () => {
+  it('should initialize errors and validatedData properties with null', () => {
+    const sut = new StubClassValidatorFields();
+
+    expect(sut.errors).toBeNull();
+    expect(sut.validatedData).toBeNull();
+  });
+
+  it('should validate with errors', () => {
+    const spyValidateSync = jest.spyOn(ClassValidator, 'validateSync');
+    spyValidateSync.mockReturnValue([
+      { property: 'field', constraints: { isRequired: 'test error' } },
+    ]);
+    const sut = new StubClassValidatorFields();
+
+    expect(sut.validate(null)).toBeFalsy();
+    expect(spyValidateSync).toHaveBeenCalled();
+    expect(sut.validatedData).toBeNull();
+    expect(sut.errors).toStrictEqual({ field: ['test error'] });
+  });
+});
