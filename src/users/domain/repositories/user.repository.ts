396839f4bbcb2a847def9332cb7f487e813691e5
Caique Daniel <caiquedaniel@outlook.com.br>
2023-11-26 23:37:21 -0300

    refactor: refactor in memory user repository contracts

--- src/users/domain/repositories/user.repository.ts
+++ src/users/domain/repositories/user.repository.ts
@@ -1,7 +1,25 @@
 import { User } from '../entities/user.entity';
-import { SearchableRepositoryInterface } from 'src/@shared/domain/repositories/searchable-repository-contracts';
+import {
+  SearchParams as DefaultSearchParams,
+  SearchResult as DefaultSearchResult,
+  SearchableRepositoryInterface,
+} from 'src/@shared/domain/repositories/searchable-repository-contracts';
 
-export interface UserRepository extends SearchableRepositoryInterface<User, any, any> {
-  findByEmail(email: string): Promise<User>;
-  emailExists(email: string): Promise<void>;
+export namespace UserRepository {
+  export type Filter = string;
+
+  export class SearchParams extends DefaultSearchParams<Filter> {}
+
+  export class SearchResult extends DefaultSearchResult<User, Filter> {}
+
+  export interface Repository
+    extends SearchableRepositoryInterface<
+      User,
+      Filter,
+      SearchParams,
+      SearchResult
+    > {
+    findByEmail(email: string): Promise<User>;
+    emailExists(email: string): Promise<void>;
+  }
 }
--- src/users/infrastructure/database/in-memory/repositories/in-memory-user.repository.ts
+++ src/users/infrastructure/database/in-memory/repositories/in-memory-user.repository.ts
@@ -1,24 +1,50 @@
 import { ConflictError } from 'src/@shared/domain/errors/conflict-error';
 import { NotFoundError } from 'src/@shared/domain/errors/not-found-error';
 import { InMemorySearchableRepository } from 'src/@shared/domain/repositories/in-memory-searchable.repository';
+import { SortDirection } from 'src/@shared/domain/repositories/searchable-repository-contracts';
 import { User } from 'src/users/domain/entities/user.entity';
 import { UserRepository } from 'src/users/domain/repositories/user.repository';
 
-export class InMemoryUserRepository
-  extends InMemorySearchableRepository<User, any, any>
-  implements UserRepository
+export class UserInMemoryRepository
+  extends InMemorySearchableRepository<User>
+  implements UserRepository.Repository
 {
+  sortableFields: string[] = ['name', 'createdAt']
+
   async findByEmail(email: string): Promise<User> {
-    const entity = this.items.find((item) => item.email == email);
-
-    if (!entity)
-      throw new NotFoundError(`Entity not found using email ${email}`);
-
-    return entity;
+    const entity = this.items.find(item => item.email === email)
+    if (!entity) {
+      throw new NotFoundError(`Entity not found using email ${email}`)
+    }
+    return entity
   }
 
   async emailExists(email: string): Promise<void> {
-    if (this.items.find((item) => item.email == email))
-      throw new ConflictError('Email address already used');
+    const entity = this.items.find(item => item.email === email)
+    if (entity) {
+      throw new ConflictError('Email address already used')
+    }
+  }
+
+  protected async applyFilter(
+    items: User[],
+    filter: UserRepository.Filter,
+  ): Promise<User[]> {
+    if (!filter) {
+      return items
+    }
+    return items.filter(item => {
+      return item.props.name.toLowerCase().includes(filter.toLowerCase())
+    })
+  }
+
+  protected async applySort(
+    items: User[],
+    sort: string | null,
+    sortDir: SortDirection | null,
+  ): Promise<User[]> {
+    return !sort
+      ? super.applySort(items, 'createdAt', 'desc')
+      : super.applySort(items, sort, sortDir)
   }
 }
