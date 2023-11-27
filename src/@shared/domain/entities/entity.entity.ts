c8e64d72ccbf979d73d2c0b796750a307dcd1161
Caique Daniel <caiquedaniel@outlook.com.br>
2023-11-26 23:30:11 -0300

    test: add tests for methods in InMemorySearchableRepository class

--- src/@shared/domain/entities/entity.entity.ts
+++ src/@shared/domain/entities/entity.entity.ts
@@ -3,7 +3,7 @@ import crypto from 'node:crypto';
 export abstract class Entity<P = any> {
   private readonly _id: string;
 
-  constructor(protected readonly props: P, id?: string) {
+  constructor(readonly props: P, id?: string) {
     this._id = id ?? crypto.randomUUID();
   }
 
